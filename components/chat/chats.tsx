e5700266b4d378c1359d61005ca2edb53f82edc4
roshanharry <roshandhoni202@gmail.com>
2024-06-04 14:36:43 +0530

    designed the dynamic chat page by adding send messages, input, emoji popover, deletemessages etc...

--- components/chat/chats.tsx
+++ components/chat/chats.tsx
@@ -2,10 +2,8 @@ import { auth } from "@/auth"
 import { getUsersForSidebar } from "@/lib/data";
 import Chat from "./chat";
 
-const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
 const Chats = async () => {
     const session = await auth();
-    await sleep(3000);
     const chats = session?.user ? await getUsersForSidebar(session.user._id) : [];
     console.log(chats);
   return (
