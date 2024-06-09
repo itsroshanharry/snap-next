57da001a224ee9e31cec7fce7b01bb75f8e221ba
roshanharry <roshandhoni202@gmail.com>
2024-06-09 16:56:42 +0530

    changed logout

--- components/shared/logout-button.tsx
+++ components/shared/logout-button.tsx
@@ -1,23 +1,26 @@
-"use client";
 import { LogOut } from "lucide-react";
 import { Button } from "../ui/button";
 import { logoutAction } from "@/lib/actions";
 import { useFormState, useFormStatus } from "react-dom";
 
-
 const LogoutButton = () => {
-	const {pending} = useFormStatus();
-	const [errorMessage, dispatch] = useFormState(logoutAction, "");
+	const { pending } = useFormStatus();
+	const [, dispatch] = useFormState(logoutAction); // Removed errorMessage since there's no initial state
+
+	const handleSubmit = async (event) => {
+		event.preventDefault(); // Prevent form submission
+		await dispatch(); // Dispatch the logout action
+	};
 
 	return (
 		<>
-		<form action={dispatch}>
-			<Button className='bg-black text-white rounded-full p-3 text-xs md:text-sm' disabled = {pending} aria-disabled={pending}>
-				<LogOut className='cursor-pointer' />
-			</Button>
-		</form>
-		{errorMessage ? <p className="text-sm text-red-500">{errorMessage}</p> : null}
+			<form onSubmit={handleSubmit}>
+				<Button className='bg-black text-white rounded-full p-3 text-xs md:text-sm' disabled={pending} aria-disabled={pending}>
+					<LogOut className='cursor-pointer' />
+				</Button>
+			</form>
 		</>
 	);
 };
+
 export default LogoutButton;
